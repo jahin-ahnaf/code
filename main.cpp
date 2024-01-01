db206dc57c5d44f5f3329fad2c8acfdba12a2b18
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-01 14:07:59 +0600

    a

--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@ void largest_num(){
     int arrayLength = sizeof(arr) / sizeof(int);
     for (int i = 0; i < arrayLength; i++){
         if (arr[i] > arr[i + 1]){
-            swap(arr[i], arr[i + 1]);
+            arr[i] = arr[i];
         }
+        else {
+            arr[i + 1] = arr[i];
+        }
+        cout << arr[i];
     }
-    return arr[];   
 }
 
 int main(){
-    cout << largest_num();
+    largest_num();
 }
