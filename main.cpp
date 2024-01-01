12d05f40e681eef790f6ab38fc8ed65c344d92f6
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-01 14:14:43 +0600

    a

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ void largest_num(){
     int arr[5] = {12,30,42,52,16};
     int arrayLength = sizeof(arr) / sizeof(int);
     for (int i = 0; i < arrayLength; i++){
-        if (arr[i] > arr[i + 1]){
-            arr[i] = arr[i];
-        }
-        else {
+        if (arr[i] < arr[i + 1]){
             arr[i + 1] = arr[i];
         }
-        cout << arr[i];
+        else {
+
+        }
     }
+        cout << arr[0];
 }
 
 int main(){
