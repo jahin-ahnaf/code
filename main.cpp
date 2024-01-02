658299e18c1af803f6ddcd02ee6f2bece4f99643
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-02 20:41:23 +0600

    a

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 //by the name of Allah
 
-/*
-    What is the third largest integer in the array?
-*/
 #include <iostream>
 using namespace std;
 
-void largest_num(){
-    int arr[5] = {12,30,42,52,16};
-    int arrayLength = sizeof(arr) / sizeof(int);
-    for (int i = 0; i < arrayLength; i++){
-        if (arr[i] < arr[i + 1]){
-            arr[i + 1] = arr[i];
-        }
-        else {
-
-        }
-    }
-        cout << arr[0];
-}
-
 int main(){
-    largest_num();
+    int n;
+    string a;
+
+    cin >> n;
+    int count = n;
+    for (int i = 0; i < n; i++){
+        if (a[i] == "1"){
+            count++;
+        }
+        else
+    }
 }
