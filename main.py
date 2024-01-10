874fa6ce0a4520bcd974dc7fc9cd95f12a75b848
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-10 17:27:07 +0600

    a

--- main.py
+++ main.py
@@ -15,29 +15,17 @@ player = playerImg.get_rect()
 player.x = 100
 player.y = 100
 
-speedX = 5
+speedX = 0
 speedY = 0
 
 while True:
-    player.x += speedX
-    player.y += speedY
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    if player.x >= 450:
-        speedX = 0
-        speedY = 5
-        
-    if player.y >= 450:
-        speedX = -5
-        speedY = 0
-
-    if player.x < 50:
-        speedX = 5
-        speedY = -5
+    
     
     display.fill((255,255,255))
     display.blit(playerImg, player)
