81c4a4b3f3e0d00d94d30ac82b40d491c371f56c
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-11 23:50:06 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,28 @@
 public class App {
+    public static int digital_root(int n) {
+        int sum = 0;
+        char[] a = String.valueOf(n).toCharArray();
+
+        int[] newArray = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            newArray[i] = Integer.parseInt(String.valueOf(a[i]));
+            sum += newArray[i];
+        }
+        String sumString = String.valueOf(sum).toString(); //29
+        if (sumString.length() > 1){
+        while (sumString.length() > 1){
+            sum = 0;
+            char[] sumChar = String.valueOf(sumString).toCharArray();
+            int[] sumInt = new int[sumChar.length];
+            for (int i = 0; i < sumInt.length; i++){
+                sum += sumInt[i];
+            }
+        }
+    }
+        System.out.println(sum);
+        return n;
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        digital_root(493193);
     }
 }
