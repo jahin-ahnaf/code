fce8959215a7972d003eadead05796cb757c80b3
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-16 21:00:43 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
-       
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+          System.out.println(i);
+        }
     }
 }
