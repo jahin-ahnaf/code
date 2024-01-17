de6732af7627f2d98d749de6b72875a969a08e61
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-17 19:10:41 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,23 +1,10 @@
 public class App {
-    public String toJadenCase(String phrase) {
-        String mew = "";
-		if (phrase.length() > 0) {
-            String[] bruh = phrase.split("");
-            int i = 0;
-            while (i < phrase.length()) {
-                bruh[0] = bruh[0].toUpperCase();
-                if (bruh[i].isEmpty()){
-                    bruh[i + 1] = bruh[i + 1].toUpperCase();
-                }
-                mew = mew + bruh[i];
-                i++;
-            }
-            return mew;
-        }
-		return null;
-	}
+    public static int sum(int[] numbers)
+  {
+    
+    return 0;
+  }
     public static void main(String[] args) {
-        App jadencase = new App();
-        System.out.println(jadencase.toJadenCase("hello world"));
+        
     }
 }
