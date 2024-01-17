3f386a9cdaa6813e7f50747f8f4ea58f085caff8
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-17 19:13:31 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class App {
     public static int sum(int[] numbers)
   {
-    
+    Arrays.sort(numbers);
+    if (numbers.length > 1){
+        
+    }
     return 0;
   }
     public static void main(String[] args) {
