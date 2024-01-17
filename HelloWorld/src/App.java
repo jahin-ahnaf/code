550f2b1b2a688e172532d1126db6a497ec94f96d
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-17 10:52:07 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,14 +1,9 @@
 public class App {
+    public String toJadenCase(String phrase) {
+		
+		return null;
+	}
     public static void main(String[] args) {
-        String originalStr = "Hello";
-        String reversedStr = "";
-        System.out.println("Original string: " + originalStr);
         
-        for (int i = 0; i < originalStr.length(); i++) {
-            reversedStr = originalStr.charAt(i) + reversedStr;
-            System.out.println(reversedStr);
-        }
-    
-        System.out.println("Reversed string: "+ reversedStr);
     }
 }
