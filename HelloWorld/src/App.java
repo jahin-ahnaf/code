cd7748231bdea2a78f89d31b899084e9babb6688
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-17 19:16:12 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -4,8 +4,13 @@ public class App {
     public static int sum(int[] numbers)
   {
     Arrays.sort(numbers);
+    int ans, i = 0;
     if (numbers.length > 1){
-        
+        do {
+            ans = numbers[i];
+        }
+        while (numbers.length < i);
+        return ans;
     }
     return 0;
   }
