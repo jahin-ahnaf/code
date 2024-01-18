12e96c03420aa8b5dd79d47fdeeefb3b6eb19718
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-18 20:51:43 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,18 +1,24 @@
-import java.util.ArrayList;
-
 public class App {
   public static String accum(String s) {
     // your code
     int count = 0;
     s = s.toLowerCase();
-    String ans = "";
-    ArrayList<String> list = new ArrayList<String>();
+    char[] bruh = s.toCharArray();
+    StringBuilder ans = new StringBuilder();
     for (int i = 0; i < s.length(); i++) {
         count++;
+        ans.append(Character.toUpperCase(bruh[i]));
+        for (int j = 1; j < count; j++){
+          Character.toLowerCase(bruh[i]);
+          ans.append(bruh[i]);
+        }
+        if(i < s.length() - 1){
+          ans.append("-");
+        }
     }
-    return ans;
+    return ans.toString();
    }
     public static void main(String[] args) {
-        System.out.println(accum("mew"));
+        System.out.println(accum("abcde"));
     }
 }
