f7383189d98e3194e79b144920caba0b19d85870
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-18 19:44:12 +0600

    hehe

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,12 +1,23 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class App {
-    public static List<Object> filterList(final List<Object> list) {
-    // Return the List with the Strings filtered out
-    
-    return null;
-  }
+  public static String repeatStr(final int repeat, final String string) {
+    String ans = "";
+    ArrayList<String> list = new ArrayList<String>();
+    for (int i = 0; i < repeat; i++) {
+        list.add(string);
+    }
+    for (int i = 0; i < list.size(); i++){
+      ans += list.get(i);
+    }
+    if (string.length() > 0 && repeat > 0){
+      return ans;
+    }
+    else {
+      return "";
+    }
+}
     public static void main(String[] args) {
-        List<String> filtered = App.filterList(List.of(1, 2, "1", "2"));
+        System.out.println(repeatStr(8, "Hello"));
     }
 }
