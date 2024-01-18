d5a2ed6509e5b9751ad8226138d502bea2ff53e7
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-18 19:47:16 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,21 +1,8 @@
 import java.util.ArrayList;
 
 public class App {
-  public static String repeatStr(final int repeat, final String string) {
-    String ans = "";
-    ArrayList<String> list = new ArrayList<String>();
-    for (int i = 0; i < repeat; i++) {
-        list.add(string);
-    }
-    for (int i = 0; i < list.size(); i++){
-      ans += list.get(i);
-    }
-    if (string.length() > 0 && repeat > 0){
-      return ans;
-    }
-    else {
-      return "";
-    }
+  public static Double multiply(Double a, Double b) {
+    return a * b;
 }
     public static void main(String[] args) {
         System.out.println(repeatStr(8, "Hello"));
