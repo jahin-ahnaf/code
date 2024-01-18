8fd67253b0a819b2c4ecb2320cc54d5ca39bb3e4
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-18 19:56:17 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 public class App {
   public static String accum(String s) {
     // your code
+    int count = 0;
+    s = s.toLowerCase();
     String ans = "";
     ArrayList<String> list = new ArrayList<String>();
-    
+    for (int i = 0; i < s.length(); i++) {
+        count++;
+    }
     return ans;
    }
     public static void main(String[] args) {
