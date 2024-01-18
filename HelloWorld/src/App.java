e31d39ae705f68aa135e691d08d7933dd34c19ef
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-18 19:09:58 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,22 +1,12 @@
+import java.util.List;
+
 public class App {
-    public static String correct(String string) {
-        String ans = "";
-        char[] bruh = string.toCharArray();
-        for (int i = 0; i < bruh.length; i++){
-            if (bruh[i] == '1'){
-                bruh[i] = 'I';
-            }
-            if (bruh[i] == '5'){
-                bruh[i] = 'S';
-            }
-            if (bruh[i] == '0'){
-                bruh[i] = 'O';
-            }
-            ans += bruh[i];
-        }
-        return ans;
-      }
+    public static List<Object> filterList(final List<Object> list) {
+    // Return the List with the Strings filtered out
+    
+    return null;
+  }
     public static void main(String[] args) {
-        System.out.println(correct("UR5ULA K. LE GU1N - A W1ZARD 0F EARTH5EA"));
+        List<String> filtered = App.filterList(List.of(1, 2, "1", "2"));
     }
 }
