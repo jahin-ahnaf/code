e7b0e4c9feb5840340ac167a4d69dc21b475eb24
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-22 19:58:27 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -2,8 +2,13 @@ public class App {
   public static boolean validatePin(String pin) {
     // Your code here...
     boolean result = false;
-    if ((pin.length() == 4) || (pin.length() == 4)){
+    if ((pin.length() == 4) || (pin.length() == 6)){
       result = true;
+    }
+    for (int i = 0; i < pin.length(); i++){
+      if (!Character.isDigit(pin.charAt(i))){
+        result = false;
+      }
     }
     return result;
   }
