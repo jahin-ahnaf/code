e3e582c1dbd2324ddc7537492d03e1d526cf093e
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-22 19:54:00 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,22 +1,11 @@
-import java.io.CharArrayReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-
 public class App {
-  public static double sum(double[] numbers) {
-    double sum = 0;
-    if (numbers.length == 0) {
-      sum = 0;
+  public static boolean validatePin(String pin) {
+    // Your code here...
+    boolean result = false;
+    if ((pin.length() == 4) || (pin.length() == 4)){
+      result = true;
     }
-    else {
-      for (int i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-      }
-    }
-    return sum;
+    return result;
   }
 public static void main(String[] args){
   
