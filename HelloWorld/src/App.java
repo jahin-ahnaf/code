6076e15ddb0257904ad3488926696cc00382198b
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-23 20:20:29 +0600

    done

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+
 public class App {
   public static String[] solution(String s) {
     //Write your code here
+    String[] arr = null;
+    if (s.length() % 2 == 0) {
+      arr = new String[s.length()];
+      for (int i = 0; i < s.length(); i++) {
+        arr[i] = s.substring(i, i+1);
+      }
+    }
+    return arr;
 }
 public static void main(String[] args){
-  
+  System.out.println(Arrays.toString(solution("abcdef")));
 }
 }
