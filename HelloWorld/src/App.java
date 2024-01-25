333c9bd71bfc1813558a55ee943caf204f3701bf
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-01-25 12:01:07 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,16 +1,9 @@
 import java.util.Arrays;
 
 public class App {
-  public static String[] solution(String s) {
-    //Write your code here
-    String[] arr = null;
-    if (s.length() % 2 == 0) {
-      arr = new String[s.length()];
-      for (int i = 0; i < s.length() / 2; i++) {
-        arr[i] = String.valueOf(s.charAt(i)) + String.valueOf(s.charAt(i + 1));
-      }
-    }
-    return arr;
+  public static String longest (String s1, String s2) {
+    // your code
+    
 }
 public static void main(String[] args){
   System.out.println(Arrays.toString(solution("abcdef")));
