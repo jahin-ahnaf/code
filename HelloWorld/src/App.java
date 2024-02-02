d8c2ca07d90ae17bb6c067cc74b227c500a2aebf
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-02-02 19:41:47 +0600

    a

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,13 +1,27 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
 
 public class App {
-  public static String MakeUpperCase(String str){
-    String upperCase = str.toUpperCase();
-    return upperCase;
+  public static List<Object> filterList(final List<Object> list) {
+    int count = 0;
+    // Return the List with the Strings filtered out
+    ArrayList<Integer> indexes = new ArrayList<Integer>();
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i) instanceof String) {
+        indexes.add(i);
+      }
+      
+    }
+    for (int i = 0; i < indexes.size(); i++){
+      list.remove(indexes.get(i) - count);
+      count++;
+      
+    }
+    return list;
   }
 public static void main(String[] args){
-  System.out.println(MakeUpperCase("wdjwndla"));
+  List<Object> list = new ArrayList<Object>(Arrays.asList(1 , 2 , "a", "b"));
+  System.out.println(App.filterList(list));
 }
 }
